885b41144b507288418ea4ef5088c89b7787fee0
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-25 20:01:03 +0200

    done

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,18 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        System.arraycopy(array, 0, array, 1, array.length - 1);
+        array[0] = array.length - 1;
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        for (int i = 0; i < shift; i++) {
+            int last = array[array.length - 1];
+            for (int j = array.length - 1; j > 0; j--) {
+                array[j] = array[j - 1];
+            }
+            array[0] = last;
+        }
     }
 }
+
