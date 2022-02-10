2bde82a1ade03fe470ecbb7132b540c4aaa4fb5d
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-10 14:38:07 +0200

    done

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -3,20 +3,37 @@ package com.epam.rd.autotasks;
 class CycleSwap {
 
     static void cycleSwap(int[] array) {
-        if (array.length > 0 && array != null) {
-            System.arraycopy(array, 0, array, 1, array.length - 1);
-            array[0] = array.length - 1;
+
+        if(array==null|| array.length==0)return;
+        else
+        {
+            int temp=array[array.length-1];
+            int[] array1=new int[array.length];
+            System.arraycopy(array,0,array1,0,array.length);
+            for (int i=1;i<array.length;i++)
+            {
+                array[i]=array1[i-1];
+            }
+            array[0]=temp;
         }
     }
 
     static void cycleSwap(int[] array, int shift) {
-        for (int i = 0; i < shift; i++) {
-            if (array.length > 0 && array != null) {
-                int temp = array[array.length - 1];
-                for (int j = array.length - 1; j > 0; j--) {
-                    array[j] = array[j - 1];
-                }
-                array[0] = temp;
+
+        if(array==null || array.length==0)return;
+        else
+        {
+            int[] array1=new int[shift];
+            int[] copyArray=new int[array.length];
+            System.arraycopy(array,0,copyArray,0,array.length);
+            System.arraycopy(array,array.length-shift,array1,0,shift);
+            for(int i=0;i<array.length-shift;i++)
+            {
+                array[i+shift]=copyArray[i];
+            }
+            for(int i=0;i<array1.length;i++)
+            {
+                array[i]=array1[i];
             }
         }
     }
